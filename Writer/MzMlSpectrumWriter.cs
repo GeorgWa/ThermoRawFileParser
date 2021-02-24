e00d3419a9d0196038797fe2f7c2ae7fc0a9afcb
Vladimir Gorshkov <vgor@bmb.sdu.dk>
2021-02-24 20:54:34 +0100

    Two methods added and isolotaionwidth bug fixed

--- Writer/MzMlSpectrumWriter.cs
+++ Writer/MzMlSpectrumWriter.cs
@@ -1151,7 +1151,9 @@ namespace ThermoRawFileParser.Writer
             int? charge = null;
             double? monoisotopicMz = null;
             double? ionInjectionTime = null;
+            double? isolationWidth = null;
             List<double> SPSMasses = new List<double>();
+
             for (var i = 0; i < trailerData.Length; i++)
             {
                 if (trailerData.Labels[i] == "Charge State:")
@@ -1160,6 +1162,11 @@ namespace ThermoRawFileParser.Writer
                     {
                         charge = Convert.ToInt32(trailerData.Values[i]);
                     }
+                }
+
+                if (trailerData.Labels[i] == "MS" + (int)scanFilter.MSOrder + " Isolation Width:")
+                {
+                    isolationWidth = double.Parse(trailerData.Values[i], NumberStyles.Any);
                 }
 
                 if (trailerData.Labels[i] == "Monoisotopic M/Z:")
@@ -1235,7 +1242,7 @@ namespace ThermoRawFileParser.Writer
 
                     // Construct and set the precursor list element of the spectrum                    
                     var precursorListType =
-                        ConstructPrecursorList(scanEvent, charge, scanFilter.MSOrder, monoisotopicMz, SPSMasses);
+                        ConstructPrecursorList(scanEvent, charge, scanFilter.MSOrder, monoisotopicMz, isolationWidth, SPSMasses);
                     spectrum.precursorList = precursorListType;
                     break;
                 case MSOrderType.Ms3:
@@ -1246,7 +1253,7 @@ namespace ThermoRawFileParser.Writer
                         name = "MSn spectrum",
                         value = ""
                     });
-                    precursorListType = ConstructPrecursorList(scanEvent, charge, scanFilter.MSOrder, monoisotopicMz, SPSMasses);
+                    precursorListType = ConstructPrecursorList(scanEvent, charge, scanFilter.MSOrder, monoisotopicMz, isolationWidth, SPSMasses);
                     spectrum.precursorList = precursorListType;
                     break;
                 default:
@@ -1785,9 +1792,10 @@ namespace ThermoRawFileParser.Writer
         /// <param name="msLevel">the MS level</param>
         /// <param name="monoisotopicMz">the monoisotopic m/z value</param>
         /// <param name="isolationWidth">the isolation width</param>
+        /// <param name="SPSMasses">masses selected for SPS</param>
         /// <returns>the precursor list</returns>
         private PrecursorListType ConstructPrecursorList(IScanEventBase scanEvent, int? charge, MSOrderType msLevel,
-            double? monoisotopicMz, List<double> SPSMasses)
+            double? monoisotopicMz, double? isolationWidth, List<double> SPSMasses)
         {
             // Construct the precursor
             var precursorList = new PrecursorListType
@@ -1800,7 +1808,7 @@ namespace ThermoRawFileParser.Writer
             int precursorScanNumber = _precursorMs1ScanNumber;
             IReaction reaction = null;
             var precursorMz = 0.0;
-            double? isolationWidth = null;
+            
             try
             {
                 switch (msLevel)
@@ -1830,7 +1838,6 @@ namespace ThermoRawFileParser.Writer
                 }
 
                 precursorMz = reaction.PrecursorMass;
-                isolationWidth = reaction.IsolationWidth;
             }
             catch (ArgumentOutOfRangeException)
             {
@@ -1874,23 +1881,24 @@ namespace ThermoRawFileParser.Writer
             }
  
             //Precursor intensity is disabled for now
-            //if (selectedIonMz > ZeroDelta)
-            //{
-            //    var selectedIonIntensity = CalculatePrecursorPeakIntensity(_rawFile, precursorScanNumber, selectedIonMz);
-            //    if (selectedIonIntensity != null)
-            //    {
-            //        ionCvParams.Add(new CVParamType
-            //        {
-            //            name = "peak intensity",
-            //            value = selectedIonIntensity.ToString(),
-            //            accession = "MS:1000042",
-            //            cvRef = "MS",
-            //            unitAccession = "MS:1000131",
-            //            unitCvRef = "MS",
-            //            unitName = "number of detector counts"
-            //        });
-            //    }
-            //}
+            if (selectedIonMz > ZeroDelta)
+            {
+                var selectedIonIntensity = CalculatePrecursorPeakIntensity(_rawFile, precursorScanNumber, selectedIonMz, isolationWidth, ParseInput.NoPeakPicking); //new Pwiz way
+                //var selectedIonIntensity = CalculatePrecursorPeakIntensity2(_rawFile, precursorScanNumber, selectedIonMz, isolationWidth); //old Pwiz way
+                if (selectedIonIntensity != null)
+                {
+                    ionCvParams.Add(new CVParamType
+                    {
+                        name = "peak intensity",
+                        value = selectedIonIntensity.ToString(),
+                        accession = "MS:1000042",
+                        cvRef = "MS",
+                        unitAccession = "MS:1000131",
+                        unitCvRef = "MS",
+                        unitName = "number of detector counts"
+                    });
+                }
+            }
 
             precursor.selectedIonList.selectedIon[0].cvParam = ionCvParams.ToArray();
 
